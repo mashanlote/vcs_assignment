f10320eb1a1ec7058241a79a5bc442dffdfe9b2b
Timur Mashanlo <mashanlote@gmail.com>
2023-11-24 21:20:16 +0300

    Add Readme

--- src/main/java/com/mashanlote/services/WeatherUpdater.java
+++ src/main/java/com/mashanlote/services/WeatherUpdater.java
@@ -1,6 +1,5 @@
 package com.mashanlote.services;
 
-import com.mashanlote.model.entities.WeatherObservation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
